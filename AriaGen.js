a6529fad31a59113bb8b0d25c694e2396ab319e7
leepolla <leepolla@gmail.com>
2018-01-13 19:05:59 -0800

    attr value suggestions

--- AriaGen.js
+++ AriaGen.js
@@ -36,14 +36,13 @@ var str2DOMElement = function(html) {
 var tags;
 var output;
 submitButton.addEventListener('click', function (e) {
-	tags = "";
+	
 	output = "";
 	var input = document.getElementById("originalText");	
 	
 	var handler = new Tautologistics.NodeHtmlParser
 	.DefaultHandler(function (error, dom) {
-		console.log("DOM");
-		console.log(dom)
+		tags = "";
 		tags = dom;
 		console.log(dom);
 		if (error){
@@ -166,6 +165,9 @@ function write(children) {
 			
 			if (children[tag].attribs && Object.keys(roleDict).includes(children[tag].name)) {
 				output = output + "<" + children[tag].data + " role=" + "\"" + children[tag].attribs.role + "\"";
+				
+				chooseAttr(children[tag], children[tag].attribs.role);
+				
 			}
 			else {
 			
@@ -197,24 +199,133 @@ function write(children) {
 	console.log(output);
 }
 
-/*
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Bad ARIA Example Page</title>
-	</head>
-	<body>
-		<header id="slide 1" class = "slide">
-			<h1>ARIA Example page</h1>
-		</header>
-		<a></a>
-		<article> </article>
-		<h2>This is the important content</h2>
-		<p role="button">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+function chooseAttr(child, role) {
+	var attrBox = document.getElementById("");
+	var attrSugg = "At " + child.raw + "with role = " + role + " there should be these attributes with these possible values: \n";
+	for (var attr in attributes[role]) {
+		for (var prop in values[attr]) {
+			attrSugg = attrSugg + attr + "=" + prop + "\n";
+		}
+	} 
+	attrBox.innerHTML = attrSugg;
+}
 
-		<p>
-		</p>
-	</body>
-</html>
-*/
+var attributes = {
+	"alert": ["aria-expanded"],
+	"alertdialog": ["aria-expanded", "aria-modal"],
+	"application": ["aria-activedescendant"],
+	"article": ["aria-expanded"],
+	"banner": ["aria-expanded"],
+	"button": ["aria-expanded", "aria-pressed"],
+	"checkbox": ["aria-checked", "aria-readonly"],
+	"columnheader": ["aria-sort", "aria-readonly", "aria-required", "aria-selected", "aria-expanded", "aria-colspan", "aria-colindex", "aria-rowindex", "aria-rowspan"],
+	"combobox": ["aria-controls", "aria-expanded", "aria-autocomplete", "aria-required", "aria-activedesendant", "aria-orientation"],
+	"complementary": ["aria-expanded"],
+	"contentinfo": [ "aria-expanded"],
+	"definition": [ "aria-expanded"],
+	"dialog": [ "aria-expanded", "aria-modal"],
+	"directory": [ "aria-expanded"],
+	"feed": [ "aria-expanded"],
+	"figure": [ "aria-expanded"],
+	"form": [ "aria-expanded"],
+	"grid": [ "aria-level", "aria-multiselectable", "aria-readonly", "aria-acivedescendant", "aria-expanded", "aria-colcount", "aria-rowcount"],
+	"gridcell": [ "aria-readonly", "aria-required", "aria-selected", "aria-expanded", "aria-colspan", "aria-colindex", "aria-rowindex", "aria-rowspan"],
+	"group": [ "aria-activedescendant", "aria-expanded"],
+	"list": [ "aria-expanded"],
+	"listbox": [ "aria-expanded", "aria-required", "aria-multiselectable", "aria-activedescendant", "aria-orientation"],
+	"listitem": [ "aria-expanded", "aria-level", "aria-posinset", "aria-setsize"],
+	"log": [ "aria-expanded"],
+	"main": [ "aria-expanded"],
+	"marquee": [ "aria-expanded"],
+	"menu": [ "aria-expanded", "aria-activedescendant", "aria-orientation"],
+	"menubar": [ "aria-expanded", "aria-activedescendant", "aria-orientation"],
+	"menuitem": [ "aria-posinset", "aria-setsize"],
+	"menuitemcheckbox": [ "aria-posinset", "aria-setsize", "aria-checked"],
+	"menuitemradio": [ "aria-posinset", "aria-setsize", "aria-checked", "aria-selected"],
+	"navigation": [ "aria-expanded"],
+	"note": [ "aria-expanded"],
+	"option": [ "aria-posinset", "aria-setsize", "aria-checked", "aria-selected"],
+	"presentation": [],
+	"progressbar": [ "aria-valuemax", "aria-valuemin", "aria-valuenow", "aria-valuetext"],
+	"radio": [ "aria-posinset", "aria-setsize", "aria-checked"],
+	"radiogroup": [ "aria-expanded", "aria-required", "aria-activedescendant", "aria-orientation"],
+	"region": [ "aria-expanded"],
+	"row": [ "aria-colindex", "aria-rolindex", "aria-level", "aria-selected", "aria-activedescendant", "aria-expanded"],
+	"rowgroup": [],
+	"rowheader": [ "aria-readonly", "aria-required", "aria-selected", "aria-expanded", "aria-colspan", "aria-colindex", "aria-rowindex", "aria-rowspan"],
+	"scrollbar": [ "aria-valuemax", "aria-valuemin", "aria-valuenow", "aria-valuetext", "aria-controls", "aria-orientation", "aria-expanded"],
+	"search": [ "aria-expanded"],
+	"searchbox": [ "aria-activedescendant", "aria-autocomplete", "aria-multiline", "aria-placeholder", "aria-readonly", "aria-required"],
+	"separator": [ "aria-valuemax", "aria-valuemin", "aria-valuenow", "aria-valuetext", "aria-orientation"],
+	"slider": [ "aria-valuemax", "aria-valuemin", "aria-valuenow", "aria-valuetext", "aria-orientation"],
+	"spinbutton": [ "aria-valuemax", "aria-valuemin", "aria-valuenow", "aria-valuetext", "aria-readonly", "aria-required"],
+	"status": [ "aria-expanded"],
+	"switch": [ "aria-checked", "aria-readonly"],
+	"tab": [ "aria-selected", "aria-posinset", "aria-setsize", "aria-expanded"],
+	"table": [ "aria-colcount", "aria-rowcount"],
+	"tablist": [ "aria-level", "aria-activedescendant", "aria-multiselectable", "aria-orientation"],
+	"tabpanel": [ "aria-expanded"],
+	"term": [ "aria-expanded"],
+	"textbox": [ "aria-activedescendant", "aria-autocomplete", "aria-multiline", "aria-placeholder", "aria-readonly", "aria-required"],
+	"timer": [ "aria-expanded"],
+	"toolbar": [ "aria-expanded", "aria-activedescendant", "aria-orientation"],
+	"tooltip": [ "aria-expanded"],
+	"tree": [ "aria-multiselectable", "aria-required", "aria-activedesendant", "aria-expanded", "aria-orientation"],
+	"treegrid": [ "aria-level", "aria-multiselectable", "aria-readonly", "aria-activedescendant", "aria-expanded", "aria-required", "aria-orientation", "aria-colcount", "aria-rowcount"],
+	"treeitem": [ "aria-level", "aria-expanded", "aria-selected", "aria-checked", "aria-posinset", "aria-setsize"]
+}
+
+var values = {
+    "aria-activedescendant": ["true", "false"],
+	"aria-atomic": ["true", "false"],
+	"aria-autocomplete": ["inline", "list", "both", "none"],
+	"aria-busy": ["true", "false"],
+	"aria-checked": ["true", "false", "mixed", "undefined"],
+	"aria-colcount": "integer",
+	"aria-colindex": "integer",
+	"aria-colspan": "integer",
+	"aria-controls": "referenced element ID",
+	"aria-current": ["page", "step", "location", "true", "false", "date", "time"],
+	"aria-describedby": "referenced element ID",
+	"aria-details": "referenced element ID",
+	"aria-disabled": ["true", "false"],
+	"aria-dropeffect":["copy", "execute", "link", "move", "none", "popup"],
+	"aria-errormessage": "referenced element ID",
+	"aria-expanded": ["true", "false", "undefined"],
+	"aria-flowto": "referenced element ID",
+	"aria-grabbed": ["true", "false", "undefined"],
+	"aria-haspopup": ["true", "false", "menu", "listbox", "grid", "dialog", "tree"],
+	"aria-hidden": ["true", "false", "undefined"],
+	"aria-invalid":["true", "false", "grammar", "spelling"],
+	"aria-keyshortcuts": "String of keyboard shortcuts",
+	"aria-label": "String to label element",
+	"aria-labelledby": "referenced element ID",
+	"aria-level":  "integer",
+	"aria-live":["assertive", "polite", "off"],
+	"aria-modal": ["true", "false"],
+	"aria-multiline": ["true", "false"],
+	"aria-multiselectable": ["true", "false"],
+	"aria-orientation": ["horizontal", "vertical", "undefined"],
+	"aria-owns": "referenced element ID",
+	"aria-placeholder": "placeholder string",
+	"aria-posinset": "integer",
+	"aria-pressed": ["true", "false", "mixed", "undefined"],
+	"aria-readonly": ["true", "false"],
+	"aria-relevant": ["additions", "additions text", "all", "text", "removals"],
+	"aria-required": ["true", "false"],
+	"aria-roledescription": "String description",
+	"aria-rowcount": "integer",
+	"aria-rowindex": "integer",
+	"aria-rowspan": "integer",
+	"aria-selected": ["true", "false", "undefined"],
+	"aria-setsize": "integer",
+	"aria-sort": ["ascending", "descending", "other", "none"],
+	"aria-valuemax": "integer",
+	"aria-valuemin": "integer",
+	"aria-valuenow": "integer",
+	"aria-valuetext": "Readable version of aria-valuenow"
+}
+
+
+
 
